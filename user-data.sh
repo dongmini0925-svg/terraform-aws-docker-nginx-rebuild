56a939b54fb0a0a162e251cac8dd605c578dbb23
Dongmin Lee <dongmini0925@gmail.com>
2026-07-22 15:41:34 +0900

    Add GitHub Actions CI CD deployment

--- user-data.sh
+++ user-data.sh
@@ -6,8 +6,15 @@ apt-get install -y docker.io
 
 systemctl enable --now docker
 
+until docker info >/dev/null 2>&1; do
+  sleep 2
+done
+
+docker pull ldm2010/my-nginx-site:latest
+docker rm -f my-nginx || true
+
 docker run -d \
   --name my-nginx \
   --restart unless-stopped \
   -p 80:80 \
-  nginx
+  ldm2010/my-nginx-site:latest
